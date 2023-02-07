dd42c875a0c0b4a4c68d08030a396c79cd6a859d
interc0der <intercoder@whirled.io>
2023-02-06 16:14:18 -0800

    Update: remove unnecessary packages, publish revised docs

--- packages/xrpl-collections/src/constants/index.ts
+++
@@ -1,9 +0,0 @@
-export const documentHeader = {
-  document: {
-    title: 'xrpl-network-registry',
-    description:
-      'This is a friendly registry of all known networks within the XRP Ledger ecoystem.',
-    maintainer: 'interc0der',
-    repo: 'https://github.com/interc0der/xrpl-network-registry.git',
-  },
-};
--- packages/xrpl-collections/src/types/index.ts
+++
@@ -1,4 +0,0 @@
-import Registry, { Network } from './networks';
-import { NetworkObjType, NetworkType } from './gen';
-
-export { Registry, Network, NetworkObjType, NetworkType };
